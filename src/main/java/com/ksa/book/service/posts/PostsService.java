f8d660008944383b8d54d320ffccbef2d1d1fc8f
ksa <ksa@opticis.com>
2021-03-29 07:15:51 +0900

    domain model add (controller, dto, service)

---
+++ src/main/java/com/ksa/book/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.ksa.book.service.posts;
+
+import com.ksa.book.domain.posts.PostsRepository;
+import com.ksa.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto)
+    {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/ksa/book/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.ksa.book.web;
+
+import com.ksa.book.service.posts.PostsService;
+import com.ksa.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto)
+    {
+        return postsService.save(requestDto);
+    }
+
+//    @PutMapping("/api/v1/posts/{id}")
+//    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequest Dto)
+//    {
+//        return postsService.update(id, requestDto);
+//    }
+//
+//    @GetMapping("/api/v1/posts/{id}")
+//    public PostsResponseDto findById(@PathVariable Long id)
+//    {
+//
+//    }
+}
