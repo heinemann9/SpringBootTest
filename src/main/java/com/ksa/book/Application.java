48310966b75adbb2010150928ca20d60c6e48719
ksa <ksa@opticis.com>
2021-04-01 06:49:57 +0900

    JPA auditing automation using LocalDate and LocalDateTime (>= java 8)

--- src/main/java/com/ksa/book/Application.java
+++ src/main/java/com/ksa/book/Application.java
@@ -2,7 +2,9 @@ package com.ksa.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing activate
 @SpringBootApplication
 public class Application
 {
